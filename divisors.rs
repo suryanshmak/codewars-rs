ca057a1be12c96b472743f292cc828dd50668d5b
Suryansh Makharia <79785067+suryanshmak@users.noreply.github.com>
2022-05-27 23:14:07 +0530

    problem #1

---
+++ divisors.rs
@@ -0,0 +1,12 @@
+fn divisors(integer: u32) -> Result<Vec<u32>, String> {
+        let mut arr: Vec<u32> = vec![];
+        for i in 2..integer {
+            if integer % i == 0 {
+                arr.push(i);
+            }
+        }
+        match arr.len() {
+           0 => Result::Err(format!("{} is prime", integer)),
+           _ => Result::Ok(arr)
+        }
+}
