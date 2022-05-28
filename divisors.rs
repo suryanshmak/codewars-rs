bc7673cee1617bd6695fb9ed4722969d9ccf2e77
Suryansh Makharia <79785067+suryanshmak@users.noreply.github.com>
2022-05-28 09:42:47 +0530

    update sol

--- divisors.rs
+++ divisors.rs
@@ -1,10 +1,5 @@
 fn divisors(integer: u32) -> Result<Vec<u32>, String> {
-        let mut arr: Vec<u32> = vec![];
-        for i in 2..integer {
-            if integer % i == 0 {
-                arr.push(i);
-            }
-        }
+        let mut arr: Vec<u32> = (2..integer).filter(|x| integer % x == 0).collect();
         match arr.len() {
            0 => Err(format!("{} is prime", integer)),
            _ => Ok(arr)
