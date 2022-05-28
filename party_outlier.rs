25f7d0addba77de897896230261f64d7e884f7a7
Suryansh Makharia <79785067+suryanshmak@users.noreply.github.com>
2022-05-28 22:03:02 +0530

    update ig

--- party_outlier.rs
+++ party_outlier.rs
@@ -1,14 +1,18 @@
 fn outlier(values: &[i32]) -> i32 {
-  let sum = values.iter().take(3).map(|x| x%2).sum();
-  
-  let a = match sum {
-    0 | 1 => 1,
-    _ => 0
-  }
-  
-  values
-      .iter()
-      .find(|x| x.abs() % 2 == a)
-      .map(|x| *x)
-      .unwrap_or(0)
+    let sum = values.iter().take(3).map(|x| x % 2).sum();
+
+    let a = match sum {
+        0 | 1 => 1,
+        _ => 0
+    };
+
+    let val = values
+        .iter()
+        .find(|x| x.abs() % 2 == a);
+
+    *val.unwrap()
+}
+
+fn main() {
+    print!("{}", outlier(&[1, 3, 5, 7, 4]));
 }
